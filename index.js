631ae2fa1bb3decc20aa24a5aaeea6ad307d0a35
Jessica Ho <jessicadinhho@gmail.com>
2022-06-29 09:37:48 -0700

    commit tree 07b6133b5f1b9dd63500c7582a7e10faa9062389

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+document.querySelector('main').remove("main#main")
+const newHeader = document.createElement('h1')
+newHeader.setAttribute('id','victory')
+newHeader.textContent = "Robin is the champion"
